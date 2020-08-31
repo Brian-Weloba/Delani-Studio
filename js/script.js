fdd668db697b678f1c5b57aba4e70e81732c3259
Brian-Weloba <bweloba@gmail.com>
2020-08-31 14:07:55 +0300

    fix sliding transition

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#development-image").click(function() {
-        $("#development-image").slideDown('1000').hide('1000');
-        $("#development").show('1000');
+        $("#development-image").slideUp('1000')
+        $("#development").slideDown('1000');
     });
     $("#development").click(function() {
-        $("#development").hide('1000');
+        $("#development").slideUp('1000');
         $("#development-image").slideDown('1000');
     });
 });
 
 $(document).ready(function() {
     $("#design-image").click(function() {
-        $("#design-image").slideDown('1000').hide('1000');
-        $("#design").show('1000');
+        $("#design-image").slideUp('1000');
+        $("#design").slideDown('1000');
     });
     $("#design").click(function() {
         $("#design").slideUp('1000');
@@ -22,8 +22,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $("#product-image").click(function() {
-        $("#product-image").slideDown('1000').hide('1000');
-        $("#product").show('1000');
+        $("#product-image").slideUp('1000');
+        $("#product").slideDown('1000');
     });
     $("#product").click(function() {
         $("#product").slideUp('1000');
