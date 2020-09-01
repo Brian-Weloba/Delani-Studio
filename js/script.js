6f6a950f6ba18621895e9c26ab5c93db3bd5b919
Brian-Weloba <bweloba@gmail.com>
2020-09-01 20:32:40 +0300

    fix responsiveness

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//what we do dropdown on click
+//drop down when cicked
 $(document).ready(function() {
     $("#dev-image").click(function() {
         $("#dev-image").slideUp('1000');
@@ -32,6 +32,72 @@ $(document).ready(function() {
     });
 });
 
+//add hover effect to portfolio
+$(document).ready(function() {
+    $("#projects").mouseenter(function() {
+        $("#overlay").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects1").mouseenter(function() {
+        $("#overlay1").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay1").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects2").mouseenter(function() {
+        $("#overlay2").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay2").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects3").mouseenter(function() {
+        $("#overlay3").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay3").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects4").mouseenter(function() {
+        $("#overlay4").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay4").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects5").mouseenter(function() {
+        $("#overlay5").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay5").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects6").mouseenter(function() {
+        $("#overlay6").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay6").css("display", "none");
+    });
+});
+
+$(document).ready(function() {
+    $("#projects7").mouseenter(function() {
+        $("#overlay7").css("display", "block");
+    }).mouseleave(function() {
+        $("#overlay7").css("display", "none");
+    });
+});
+
+//alert
 $(document).ready(function() {
     $('#mc-embedded-subscribe').click(function() {
         alert($("#mce-FNAME").val() + ", we have received your message. Thank you for reaching out to us.");
